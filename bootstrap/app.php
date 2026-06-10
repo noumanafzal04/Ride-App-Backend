54f1b8e71f90f4547ad8fdf6b624401be5965114
faiz48097 <faiz48097@gmail.com>
2026-06-11 00:30:32 +0500

    minor changes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,8 +11,10 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware) {
-        //
+    ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->api([
+            \App\Http\Middleware\ApiContextMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
